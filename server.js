ca42c974ae09640c9b0b6d7e10f34ac705c9716d
Manuel Valle <vallerem17@gmail.com>
2018-04-03 00:34:54 +0200

    little graphql example working in the back end

--- server.js
+++ server.js
@@ -1,9 +1,66 @@
-const express = require('express');
-const app = express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
+const { makeExecutableSchema } = require("graphql-tools");
 const port = process.env.PORT || 7000;
 
-app.get('/api/hello', (req, res) => {
-  res.send({ Hello: 'World' });
+// Some fake data
+const books = [
+  {
+    title: "Harry Potter and the Sorcerer's stone",
+    author: "J.K. Rowling"
+  },
+  {
+    title: "Jurassic Park",
+    author: "Michael Crichton"
+  }
+];
+
+const booksMeh = [
+  {
+    title: "Harry Potter and the Sorcerer's stone",
+    author: "ddddd"
+  },
+  {
+    title: "f sd",
+    author: "sd sd"
+  }
+];
+
+// The GraphQL schema in string form
+const typeDefs = `
+  type Query { books: [Book] }
+  type Book { title: String, author: String }
+`;
+
+// The resolvers
+const resolvers = {
+  Query: { books: () => books }
+};
+
+// Put together a schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+// Initialize the app
+const app = express();
+
+// The GraphQL endpoint
+app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
+
+// GraphiQL, a visual editor for queries
+app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
+
+// extra
+app.get("/api/hello", (req, res) => {
+  res.send({ Hello: "World" });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log("Go to http://localhost:7000/graphiql to run queries!");
+});
